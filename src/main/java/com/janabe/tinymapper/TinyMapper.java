03091866854aa4460ad408dd79904445ec0c44fc
Jan Abe de Waal <jandewaal33@gmail.com>
2019-05-04 11:42:56 +0200

    Completely refactor TinyMapper

--- src/main/java/com/janabe/tinymapper/TinyMapper.java
+++ src/main/java/com/janabe/tinymapper/TinyMapper.java
@@ -1,12 +1,15 @@
 package com.janabe.tinymapper;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TinyMapper<T> {
     private Class<T> type;
@@ -16,206 +19,182 @@ public class TinyMapper<T> {
     }
 
     /**
-     * <p>Map the results from the result set to the attributes of T,
-     * creating a new instance of T.</p>
-     * @param rs result set
-     * @return Instance of T, based on the Result Set.
-     * @throws SQLException e
-     * @throws NoSuchMethodException e
-     * @throws IllegalArgumentException e
-     * @throws IllegalAccessException e
-     * @throws InstantiationException e
-     * @throws InvocationTargetException e
+     * <p>Map the provided ResultSet to an object of type T.</p>
+     * @param resultSet ResultSet containing the data
+     * @return Newly created Object of type T, filled with data
      */
-    public T map(ResultSet rs) throws Exception, SQLException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException, InstantiationException, InvocationTargetException {
-        var fieldToColumnToType = this.fieldToColumnToType();
-        var embeddedInfo = this.parseEmbedded();
-        var constructor = type.getDeclaredConstructor();
-        var instance = constructor.newInstance();
-        // variable that gets used by setters for T
-        var value = new Object();
+    public T map(ResultSet resultSet) {
+        var columnFields = getAnnotatedFields(type, Column.class);
+        var embedFields = getAnnotatedFields(type, Embed.class);
+        var instance = createInstance(type);
 
-        // all fieldNames that are marked as @Embed
-        var embeddedFields = new ArrayList<String>();
-        for (Field embeddedField : type.getDeclaredFields()) {
-            if (embeddedField.isAnnotationPresent(Embed.class)) {
-                embeddedFields.add(embeddedField.getName());
-            }
-        }
+        fillColumnFields(instance, columnFields, resultSet);
+        fillEmbedFields(instance, embedFields, resultSet);
 
-        // the attribute that is marked as @Embed, acts as a placeholder, that slowly gets build up
-        var embeddedObject = new Object();
-        // variable that gets used by setters on the embeddedObject
-        var val = new Object();
-
-        // Prepare the embeddedObject(s)
-        // loop over all keys (these are all ClassNames)
-        for (var clazz : embeddedInfo.keySet()) {
-            // get the class that belongs to the ClassName, and create a new instance
-            embeddedObject = this.classByName(clazz).getDeclaredConstructor().newInstance();
-            // get the value that belongs to key=clazz from the embeddedField
-            var ebFieldToColumnToType = embeddedInfo.get(clazz);
-            // loop over all fields of 'clazz'
-            for (var field : embeddedInfo.get(clazz).keySet()) {
-                // get the column and type map that corresponds to the 'field'
-                var columnToType = ebFieldToColumnToType.get(field);
-                // get the value from the resultSet that corresponds to the column and cast it to the correct dataType.
-                val = this.cast(columnToType, rs);
-                // get the setter method of 'field'
-                var setterName = this.setMethod(field);
-                var embeddedClass = embeddedObject.getClass();
-                var valueClass = val.getClass();
-                var setter = embeddedClass.getDeclaredMethod(setterName, valueClass);
-                setter.invoke(embeddedObject, val);
-            }
-
-            for (var field : fieldToColumnToType.keySet()) {
-                // if the field is marked as @Embed -> call the setter method corresponding to this field, with the earlier made object as argument.
-                // check if clazz contains field, otherwise the setter for field gets called multiple times with different arguments that are not part of the method signature
-                // this is complete shitt.
-                if (embeddedFields.contains(field) && clazz.toLowerCase().contains(field.toLowerCase())) {
-                    instance.getClass().getDeclaredMethod(this.setMethod(field), embeddedObject.getClass()).invoke(instance, embeddedObject);
-                }
-            }
-        }
-
-        // loop over all fields of T
-        for (var field : fieldToColumnToType.keySet()) {
-            // if the field is @Column -> call the setter method corresponding to this field on T, with the earlier made value as argument.
-            if (!embeddedFields.contains(field)) {
-                value = this.cast(fieldToColumnToType.get(field), rs);
-                type.getDeclaredMethod(this.setMethod(field), String.class).invoke(instance, value);
-            }
-        }
-
-        return instance;
+        return (T) instance;
     }
 
     /**
-     * <p>Cast a columns value from the result set to it's correct data type.
-     * Which can then be used as value for setters.</p>
-     * @param columnToType Map(Column, Type)
-     * @param rs result set
-     * @return casted object
-     * @throws SQLException e
+     * <p>Fill the fields marked with @Embed.</p>
+     * @param instance Class containing fields marked with @Embed
+     * @param embedFields List of all fields marked with @Embed
+     * @param resultSet ResultSet used to obtain data from
      */
-    private Object cast(Map<String, String> columnToType, ResultSet rs) throws SQLException {
-        var castedObject = new Object();
-        // get the column (key) from the map
-        var column = (String) columnToType.keySet().toArray()[0];
-        // get the ClassName of the type that the column has
-        var clazz = columnToType.get(column);
+    private void fillEmbedFields(Object instance, List<Field> embedFields, ResultSet resultSet) {
+        for (Field field : embedFields) {
+            var embedInstance = createInstance(field.getType());
+            fillColumnFields(embedInstance, getAnnotatedFields(field.getType(), Column.class), resultSet);
+            fillField(instance, field, embedInstance);
+        }
+    }
+
+    /**
+     * <p>Fill all provided Fields of the provided (Object) Instance with values obtained from the provided ResultSet</p>
+     * @param instance Instance of an object who's fields you want filled
+     * @param fields Fields of an instance you want filled
+     * @param resultSet ResultSet used to obtain values from
+     */
+    private void fillColumnFields(Object instance, List<Field> fields, ResultSet resultSet) {
+        for (Field field : fields) {
+            var result = transform(resultSet, field);
+            var setter = getSetterOf(instance, field, result.getClass());
+            try {
+                setter.invoke(instance, result);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * <p>Fill the field of the provided instance with the provided argument.</p>
+     * @param instance Object
+     * @param field Field
+     * @param arg Object
+     */
+    private void fillField(Object instance, Field field, Object arg) {
+        var setter = getSetterOf(instance, field, arg.getClass());
 
         try {
-            // get the value from the resultSet that corresponds to the column
-            // then cast it to the correct dataType
-            castedObject = classByName(clazz).cast(rs.getObject(column));
-        } catch (ClassCastException e) {
-            System.out.println("Exception occurred casting an object from resultSet value: " + e);
+            setter.invoke(instance, arg);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            System.out.println("Exception occurred while invoking a setter method on object " + instance.getClass().getName());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * <p>Transform the resultSet into the value that belongs to the provided columnField.</p>
+     * @param resultSet ResultSet
+     * @param columnField field marked with @Column
+     * @return the object that
+     */
+    private Object transform(ResultSet resultSet, Field columnField) {
+        var castedObject = new Object();
+        var columnName = getColumnNameOf(columnField);
+        var _class = columnField.getType();
+
+        try {
+            castedObject = _class.cast(resultSet.getObject(columnName));
+        } catch (SQLException e) {
+            System.out.println("Exception occurred casting an object from resultSet value");
+            e.printStackTrace();
         }
 
         return castedObject;
     }
 
     /**
-     * <p>Get the class based on its classname. Functions as a wrapper
-     * for Class.forName().</p>
-     * @param className className
-     * @return Class
+     * <p>Create a new instance of the provided _class.</p>
+     * @param _class Class you want a new instance from
+     * @return New instance
      */
-    private Class<?> classByName(String className) {
-        Class<?> c = null;
+    private Object createInstance(Class<?> _class) {
+        Object instance = null;
 
         try {
-            c = Class.forName(className);
-        } catch (ClassNotFoundException e) {
-            System.out.println("Exception occurred while getting class based on name: " + e);
+            instance = getEmptyConstructor(_class).newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            System.out.println("Exception occurred while creating a new instance of " + _class.getName());
+            e.printStackTrace();
         }
 
-        return c;
+        return instance;
     }
 
     /**
-     * <p> Get information about the column attributes.
-     * Get all fields, corresponding columns and corresponding data types.
-     * Column -> Map<ClassName, Map<ColumnName, TypeName>>
-     *           Map<id, Map<id, String>>
-     * </p>
-     * @return Map(Field, Map(Column, Type))
+     * <p>Get the empty constructor of the provided class.</p>
+     * @param _class Class whose empty constructor you want
+     * @return Empty constructor
      */
-    private Map<String, Map<String, String>> fieldToColumnToType() {
-        var fieldToColumnToType = new HashMap<String, Map<String, String>>();
-        String columnName;
-        String typeName;
+    private Constructor<?> getEmptyConstructor(Class<?> _class) {
+        Constructor<?> constructor = null;
 
-        for (Field field : type.getDeclaredFields()) {
-            if (field.isAnnotationPresent(Column.class)) {
-                columnName = field.getAnnotation(Column.class).name();
-                typeName = field.getType().getName();
-                fieldToColumnToType.put(field.getName(), Map.of(columnName, typeName));
-            } else if (field.isAnnotationPresent(Embed.class)) {
-                // if field is marked with @Embed -> still add it to the map with an empty map as value
-                // reason for empty map: the @Embed field doesn't directly has a column and type
-                // reason for adding it at all: the result from this method is used to find all fields from T.
-                // this is later used to find the corresponding setter methods. Might be able to remove it if a refactor happens.
-                fieldToColumnToType.put(field.getName(), Map.of());
-            }
+        try {
+            constructor = _class.getDeclaredConstructor();
+        } catch (NoSuchMethodException e) {
+            System.out.println("No empty constructor found! TinyMapper needs an empty constructor in order to work!");
+            e.printStackTrace();
         }
 
-        return fieldToColumnToType;
+        return constructor;
     }
 
     /**
-     * <p>Get information about the embedded attributes.
-     * Find all attributes marked with @Embed.
-     * Find all attributes of these attributes that are marked with @Column.
-     * Find the columnName and dataType of these attributes.
-     * e.g. :
-     * Embed -> Map<ClassName, Map<FieldName, Map<ColumnName, TypeName>>>
-     *          Map<FullName, Map<firstName, Map<first_name, String>>>
-     *          Map<FullName, Map<lastName, Map<last_name, String>>>
-     * </p>
-     * @return Map(ClassName, Map(FieldName, Map(ColumnName, TypeName)))
+     * <p>Get the setter method with the specified signature (field, arg) from the provided _class.</p>
+     * @param _class Class containing the setter method
+     * @param field Field wherefore you want to obtain the setter method
+     * @param arg Argument the setter method expects as parameter
+     * @return Setter method for Field from _Class
      */
-    // alle attributen, van alle embedded klassen, worden gekoppeld aan de fields.
-    // dus de attributen van FullName worden gekoppeld aan zowel fullName als email
-    // de attributen van email worden gekoppeld aan zowel email als fullName.
-    // fullName: firstName, lastName, emailAddress
-    // en dit klopt natuurlijk niet, het moet beschouwd worden als twee losse dingen.
-    private Map<String, Map<String, Map<String, String>>> parseEmbedded() {
-        var map = new HashMap<String, Map<String, Map<String, String>>>();
-        String columnName;
-        String typeName;
+    private Method getSetterOf(Object _class, Field field, Class<?> arg) {
+        Method method = null;
 
-        for (Field field : type.getDeclaredFields()) {
-            if (!field.isAnnotationPresent(Embed.class)) {
-                continue;
-            }
-            var fieldToColumn = new HashMap<String, Map<String, String>>();
-
-            for (Field childField : field.getType().getDeclaredFields()) {
-                if (!childField.isAnnotationPresent(Column.class)) {
-                    continue;
-                }
-                columnName = childField.getAnnotation(Column.class).name();
-                typeName = childField.getType().getName();
-                fieldToColumn.put(childField.getName(), Map.of(columnName, typeName));
-                map.put(field.getType().getName(), fieldToColumn);
-            }
+        try {
+            method = _class.getClass().getDeclaredMethod(getSetterNameOf(field.getName()), arg);
+        } catch (NoSuchMethodException e) {
+            System.out.println("Exception occurred while getting the setter method of " + _class + " for field " + field.getName());
+            e.printStackTrace();
         }
 
-        return map;
+        return method;
     }
 
     /**
-     * <p>Get the setter method from the provided field.
-     * The setter method should follow the standard conventions.
-     * e.g. setName() for an attribute with the name 'name'.</p>
-     * @param field f
-     * @return setter of field
+     * <p>Get the name of the setter method corresponding to the provided fieldName.
+     * This method is used as a helper method in getSetterOf.
+     * e.g. field: name -> setter: setName.</p>
+     * @param fieldName name of a field
+     * @return name of setter method
      */
-    private String setMethod(String field) {
-        return "set" + field.substring(0, 1).toUpperCase() + field.substring(1);
+    private String getSetterNameOf(String fieldName) {
+        return "set" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
     }
 
+    /**
+     * <p>Get the name of the column of the @Column annotated field.</p>
+     * @param columnField Field marked with @Column
+     * @return Name of the column corresponding to the provided field
+     */
+    private String getColumnNameOf(Field columnField) {
+        if (!columnField.isAnnotationPresent(Column.class)) {
+            return null;
+        }
+
+        return columnField.getAnnotation(Column.class).name();
+    }
+
+    /**
+     * <p>Get all fields that are annotated with the provided Annotation
+     * from the provided Class.</p>
+     * @param _class some Class
+     * @return a list containing all fields marked with @Column
+     */
+    private List<Field> getAnnotatedFields(Class<?> _class, Class<? extends Annotation> wantedAnnotation) {
+        var fields = Arrays.stream(_class.getDeclaredFields());
+        return fields
+                .filter(field -> field.isAnnotationPresent(wantedAnnotation))
+                .collect(Collectors.toList());
+    }
 }
+
